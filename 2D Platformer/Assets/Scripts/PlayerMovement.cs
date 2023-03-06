bc3e561c6a10d61c0fc2afc79dfecf6f243c7288
MurkesM <91104156+MurkesM@users.noreply.github.com>
2023-03-05 22:13:56 -0600

    changed jump function from lerp to translate, added sprinting.

--- 2D Platformer/Assets/Scripts/PlayerMovement.cs
+++ 2D Platformer/Assets/Scripts/PlayerMovement.cs
@@ -5,29 +5,51 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
-    [SerializeField] float move_speed = 2;
+    [SerializeField] float move_speed = 10;
+    [SerializeField] float sprint_speed = 15;
 
     [Header("Jump")]
     [SerializeField] float jump_speed = 1000;
-    
+    [SerializeField] float jump_height = 2;
+    Vector3 jump_height_vector;
+
+    void Awake()
+    {
+        jump_height_vector = new Vector3(0, jump_height, 0);
+    }
+
     void Update()
     {
-        //TODO: move using the newest button clicked
+        HandleMovement();
+        HandleJumping();
+    }
+
+    void HandleMovement()
+    {
+        //TODO: move using the newest button pressed as your direction instead of defaulting to right
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(move_speed * Time.deltaTime * Vector2.right);
+            if (Input.GetKey(KeyCode.LeftShift))
+                transform.Translate(sprint_speed * Time.deltaTime * Vector2.right);
+            else
+                transform.Translate(move_speed * Time.deltaTime * Vector2.right);
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(move_speed * Time.deltaTime * Vector2.left);
+            if (Input.GetKey(KeyCode.LeftShift))
+                transform.Translate(sprint_speed * Time.deltaTime * Vector2.left);
+            else
+                transform.Translate(move_speed * Time.deltaTime * Vector2.left);
         }
+    }
 
+    void HandleJumping()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             //need to check if on ground
-
-            transform.position = Vector2.Lerp(transform.position, transform.position + transform.up, jump_speed * Time.deltaTime);
+            transform.Translate(jump_speed * Time.deltaTime * jump_height_vector);
         }
     }
 }
