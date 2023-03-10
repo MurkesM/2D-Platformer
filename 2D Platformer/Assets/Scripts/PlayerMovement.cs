b1b9cb126da99513d3734c7d1257f2e80dfe76ca
MurkesM <91104156+MurkesM@users.noreply.github.com>
2023-03-09 23:13:40 -0600

    started on setting up animation states through code. Needs more work.

--- 2D Platformer/Assets/Scripts/PlayerMovement.cs
+++ 2D Platformer/Assets/Scripts/PlayerMovement.cs
@@ -13,8 +13,19 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float jump_height = 2;
     Vector3 jump_height_vector;
 
+    SpriteRenderer renderer;
+
+    Animator animator;
+    const string IDLE_ANIM = "Idle";
+    const string WALK_ANIM = "Walk";
+    const string RUN_ANIM = "Run";
+    const string JUMP_ANIM = "Jump";
+
     void Awake()
     {
+        renderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
         jump_height_vector = new Vector3(0, jump_height, 0);
     }
 
@@ -27,29 +38,69 @@ public class PlayerMovement : MonoBehaviour
     void HandleMovement()
     {
         //TODO: move using the newest button pressed as your direction instead of defaulting to right
+        //TODO: animations aren't setup correctly. Calling ResetTrigger fixes it but it doesn't seem like the correct route.
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            if (Input.GetKey(KeyCode.LeftShift))
-                transform.Translate(sprint_speed * Time.deltaTime * Vector2.right);
-            else
-                transform.Translate(move_speed * Time.deltaTime * Vector2.right);
-        }
+        if (Input.GetKey(KeyCode.D)) 
+            MoveRight();
         else if (Input.GetKey(KeyCode.A))
+            MoveLeft();
+        else 
+            animator.SetTrigger(IDLE_ANIM);
+    }
+
+    void MoveRight()
+    {
+        renderer.flipX = false; 
+
+        if (Input.GetKey(KeyCode.LeftShift))
         {
-            if (Input.GetKey(KeyCode.LeftShift))
-                transform.Translate(sprint_speed * Time.deltaTime * Vector2.left);
-            else
-                transform.Translate(move_speed * Time.deltaTime * Vector2.left);
+            transform.Translate(sprint_speed * Time.deltaTime * Vector2.right);
+
+            animator.SetTrigger(RUN_ANIM);
+            animator.ResetTrigger(WALK_ANIM);
         }
+            
+        else
+        {
+            transform.Translate(move_speed * Time.deltaTime * Vector2.right);
+
+            animator.SetTrigger(WALK_ANIM);
+            animator.ResetTrigger(RUN_ANIM);
+        }
+    }
+
+    void MoveLeft()
+    {
+        renderer.flipX = true;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(sprint_speed * Time.deltaTime * Vector2.left);
+
+            animator.SetTrigger(RUN_ANIM);
+            animator.ResetTrigger(WALK_ANIM);
+        }
+            
+        else
+        {
+            transform.Translate(move_speed * Time.deltaTime * Vector2.left);
+
+            animator.SetTrigger(WALK_ANIM);
+            animator.ResetTrigger(RUN_ANIM);
+        }
+            
     }
 
     void HandleJumping()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            //jumping isnt working right. jump height doesn't seem to affect anything.
+            //probably need to move to rigidi body based jumping and rigidbody based movement
             //need to check if on ground
             transform.Translate(jump_speed * Time.deltaTime * jump_height_vector);
+
+            //TODO: Play jump animation
         }
     }
 }
