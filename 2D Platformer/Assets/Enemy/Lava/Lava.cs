fe572fd0049d38d20c660b61247f1dbdcd4593d7
MurkesM <91104156+MurkesM@users.noreply.github.com>
2023-04-05 19:47:10 -0500

    created basic lava script

---
+++ 2D Platformer/Assets/Enemy/Lava/Lava.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Lava : Interactable2D
+{
+    protected override void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(playerLayer))
+        {
+            PlayerControls.KillPlayer();
+        }
+    }
+}
