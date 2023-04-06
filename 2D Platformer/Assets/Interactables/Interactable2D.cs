c40a44576e6ff2d4b25a15a73774015b3a9b1786
MurkesM <91104156+MurkesM@users.noreply.github.com>
2023-04-05 19:46:35 -0500

    made player layer variable a constant

--- 2D Platformer/Assets/Interactables/Interactable2D.cs
+++ 2D Platformer/Assets/Interactables/Interactable2D.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public abstract class Interactable2D : MonoBehaviour
 {
-    public bool is_interactable = true;
+    public const string playerLayer = "Player";
+
+    public bool isInteractable = true;
     public float timeTillInteractable = 0;
     protected abstract void OnTriggerEnter2D(Collider2D collision);
     protected virtual void OnTriggerExit2D(Collider2D collision) { }
