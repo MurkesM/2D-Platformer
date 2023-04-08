e0dbede24cc8f7719bab9fefa7ed7053484eb9b3
MurkesM <91104156+MurkesM@users.noreply.github.com>
2023-04-07 23:40:00 -0500

    projectile will now move towards the player, destroy itself after x time or hit player which will kill the player and destory the projectile.

--- 2D Platformer/Assets/Enemy/Turret/TurretProjectile.cs
+++ 2D Platformer/Assets/Enemy/Turret/TurretProjectile.cs
@@ -3,18 +3,46 @@ using UnityEngine;
 public class TurretProjectile : MonoBehaviour
 {
     [SerializeField] float move_speed = 1f;
+    Vector3 target = new();
 
-    public void SetMoveDirection(Vector3 move_direction)
+    [SerializeField] float time_to_destroy = 4;
+    float time_alive = 0;
+
+    const string player_tag = "Player";
+
+    void OnEnable()
     {
-        Vector3.MoveTowards(transform.position, move_direction, move_speed * Time.deltaTime);
+        time_to_destroy += Time.time;
     }
 
-    public void SetLookDirection(Vector3 look_direction)
+    void Update()
     {
-        transform.LookAt(look_direction);
-    } 
+        transform.position = Vector2.MoveTowards(transform.position, target * 1.75f, move_speed * Time.deltaTime);
 
-    //check if playyer, then kill player and destroy projectile
+        time_alive = Time.time;
 
-    //destory projectile after so many seconds
+        if (time_alive >= time_to_destroy)
+            DestroyProjectile();
+    }
+
+    public void SetTarget(Vector3 target)
+    {
+        this.target = target;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (CompareTag(player_tag))
+        {
+            PlayerControls.KillPlayer();
+            DestroyProjectile();
+        }
+    }
+
+    void DestroyProjectile()
+    {
+        //TODO: add sfx and/or vfx here
+
+        Destroy(gameObject);
+    }
 }
