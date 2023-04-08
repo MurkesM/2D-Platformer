d457472279cd2171bec168307eced0f80d2ce627
MurkesM <91104156+MurkesM@users.noreply.github.com>
2023-04-07 23:39:02 -0500

    turrent will now rotate and shoot at the player when in range

--- 2D Platformer/Assets/Enemy/Turret/Turret.cs
+++ 2D Platformer/Assets/Enemy/Turret/Turret.cs
@@ -3,43 +3,71 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-    [SerializeField] GameObject barrel_pivot;
+    [Header("Barrel")]
     [SerializeField] GameObject barrel;
+    [SerializeField] GameObject barrel_pivot;
+    [SerializeField] float barrel_rotation_speed = 90;
+    [SerializeField] float smoothTime = 0.3f;
+    Vector3 target_direction = new();
+    float angle;
+    float targetAngle;
+    float currentVelocity;
+
+    [Header("Projectile")]
     [SerializeField] GameObject projectile;
-    [SerializeField] float rotate_speed = 1;
+    [SerializeField] float fire_speed = 1;
+
+    Vector3 target_position = new();
 
     const string player_tag = "Player";
+
+    Coroutine fire_routine = null;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag(player_tag))
-            StartCoroutine(FireRoutine(other.transform.position));
+        {
+            target_position = other.transform.position;
+
+            fire_routine = StartCoroutine(FireRoutine());
+        }
     }
 
     void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag(player_tag))
-            barrel_pivot.transform.LookAt(other.transform.position * rotate_speed * Time.deltaTime);
+        {
+            target_position = other.transform.position;
+
+            //Determine which direction to rotate towards
+            target_direction = target_position - barrel_pivot.transform.position;
+
+            //Determine the angle in between the forward direction of the barrel pivot and the target
+            targetAngle = Vector2.SignedAngle(Vector2.down, target_direction);
+
+            //Get a smoothed out version of the angle
+            angle = Mathf.SmoothDampAngle(angle, targetAngle, ref currentVelocity, smoothTime, barrel_rotation_speed);
+
+            //rotate by angle
+            barrel_pivot.transform.eulerAngles = new Vector3(0, 0, angle);
+        }
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag(player_tag))
-            StopCoroutine(FireRoutine(Vector3.zero));
+            StopCoroutine(fire_routine);
     }
 
-    IEnumerator FireRoutine(Vector3 player_position)
+    IEnumerator FireRoutine()
     {
-        //TODO: add comments 
-
         while (true)
         {
-            GameObject new_projectile = Instantiate(projectile, barrel.transform.position, Quaternion.identity);
-            TurretProjectile turret_projectile = new_projectile.GetComponent<TurretProjectile>();
-            turret_projectile.SetMoveDirection(player_position);
-            turret_projectile.SetLookDirection(player_position);
+            GameObject new_projectile = Instantiate(projectile, barrel.transform.position, Quaternion.identity, this.transform);
 
-            yield return new WaitForSeconds(.5F);
+            new_projectile.GetComponent<TurretProjectile>().SetTarget(target_position);
+
+            yield return new WaitForSeconds(fire_speed);
         }
     }
 }
