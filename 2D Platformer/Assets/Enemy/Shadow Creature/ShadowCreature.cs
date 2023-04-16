74cc39d3c46e0cd6b33d057eb045b1f84240f560
MurkesM <91104156+MurkesM@users.noreply.github.com>
2023-04-16 00:06:24 -0500

    shadow creature will now stay at same height as player

--- 2D Platformer/Assets/Enemy/Shadow Creature/ShadowCreature.cs
+++ 2D Platformer/Assets/Enemy/Shadow Creature/ShadowCreature.cs
@@ -1,19 +1,33 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ShadowCreature : MonoBehaviour
 {
     [SerializeField] float move_speed = 2;
     bool can_move = true;
 
-    const string player_layer = "Player";
+    Vector2 move_direction = new();
 
+    const string player_layer = "Player";
     const string torch_layer = "Torch";
 
     Torch torch = null;
+    PlayerControls player_controls = null;
+
+    void Awake()
+    {
+        player_controls = FindObjectOfType<PlayerControls>();
+
+        //initialize move direciton
+        move_direction = new(1, player_controls.transform.position.y);
+    }
 
     void Update()
     {
-        transform.Translate(Vector2.right * move_speed * Time.deltaTime);
+        //update the height of the creature based on players height 
+        move_direction.y = player_controls.transform.position.y;
+
+        transform.Translate(move_direction * move_speed * Time.deltaTime);
     }
 
     void OnTriggerEnter2D(Collider2D other)
