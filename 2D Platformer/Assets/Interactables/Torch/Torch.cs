6153c8265fee0e15b0122054b8fd1df0888550b5
MurkesM <91104156+MurkesM@users.noreply.github.com>
2023-04-15 23:02:13 -0500

    gave the torch class some state handeling

--- 2D Platformer/Assets/Interactables/Torch/Torch.cs
+++ 2D Platformer/Assets/Interactables/Torch/Torch.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -6,12 +7,19 @@ public class Torch : Interactable2D
     const string anim_param = "Glow";
     [SerializeField] Animator animator;
 
+    bool is_glowing = false;
+    public bool IsGlowing { get { return is_glowing; } }
+
+    public Action<bool> GlowStateUpdated;
+
+    Coroutine glow_Routine = null;
+
     protected override void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag(playerTag))
         {
             if (animator != null && isInteractable)
-                StartCoroutine(GlowRoutine());
+                glow_Routine = StartCoroutine(GlowRoutine());
         }
     }
 
@@ -20,10 +28,21 @@ public class Torch : Interactable2D
         //play anim for set amount of time and remove the ability to trigger the anim again
         animator.SetBool(anim_param, true);
         isInteractable = false;
+
+        //set state and sent events
+        is_glowing = true;
+        GlowStateUpdated(is_glowing);
+
         yield return new WaitForSeconds(3);
 
-        //stop anim and and coroutine
+        //stop anim
         animator.SetBool(anim_param, false);
-        StopCoroutine(GlowRoutine());
+
+        //set state and sent events
+        is_glowing = false;
+        GlowStateUpdated(is_glowing);
+
+        //stop coroutine
+        StopCoroutine(glow_Routine);
     }
 }
